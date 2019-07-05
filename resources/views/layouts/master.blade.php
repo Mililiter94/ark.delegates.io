6578bb0bfb2b304c88760cc7929bf0eccb7be10e
A6Patch <klp@anaconda.net>
2019-07-05 08:18:58 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,7 +37,6 @@
     </head>
 
     <body class="font-sans">
-    <div data-type="countdown" data-id="1295618" class="tickcounter" style="width: 300px; height: 75px; margin: 0 auto"><a href="//www.tickcounter.com/countdown/1295618/qredit-delegate-update" title="Qredit Delegate Update">Qredit Delegate Update</a><a href="//www.tickcounter.com/" title="Countdown">Countdown</a></div><script>(function(d, s, id) { var js, pjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }(document, "script", "tickcounter-sdk"));</script>
         <div id="app">
             {{ $slot }}
         </div>
